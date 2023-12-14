413e968b32e0bf6c8296a7c156dad2a7ec1a63be
Raphael <raphaelmicucci@gmail.com>
2023-12-14 17:29:59 -0300

    project finished

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-const button = document.querySelector("button");
+let i = 0;
+
+const colors = ["blue", "orange", "black", "green", "yellow", "cyan", "purple"];
+
+const changeColor = () => {
+    document.querySelector("body").style.backgroundColor = colors[i++];
+
+    if (i > colors.length-1){
+        i = 0;
+    };
+};
 
